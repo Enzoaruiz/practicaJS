f335cf47b1cfdae03e7ee44bff6d81169b3428de
Enzo Ruiz <enzoruiz100398@gmail.com>
2023-10-09 17:17:05 -0300

    prueba

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 // const criticasCurso = [];
 
 let formulario = document.querySelector('#form')
-let input = document.querySelector('.input');
-let btnOk = document.querySelector('#btnOk');
-let btnCancel = document.querySelector('#btnCancel');
+let input = document.querySelector('.input')
+let title = document.querySelector('#title');
+let listOk = document.querySelector('#list-ok');
+let listCancel = document.querySelector('#listCancel');
 
-const like = () => {   
-    btnOk.addEventListener('click', (critica) =>{
-    let criticaP = input.value;
-    if  (critica !== '') {
-        criticasPositivas.push(critica);
-        } else {
-            alert('Debes de ingresar una critica');
-        }
-})
+
+
+// creamos una funcion para validar el input 
+
+function validateInput (array) {
+    return [...array].every((input) => input.value !== '');
 }
 
-const disLike = () => {
-    btnCancel.addEventListener('click', (critica) =>{
-    let criticaN = input.value;
-    if  (critica !== '') {
-    criticasNegativas.push(critica);
-    } else {
-        alert('Debes de ingresar una critica');
-    }
-})
-}
 
 // funcion constructora
 
@@ -36,36 +24,35 @@ function Curso (nombre, modalidad, division){
     this.criticasPositivas = [];
     this.criticasNegativas = [];
     this.presentacion = function(){
-        // console.log(`Bienvenidos al curso de ${this.nombre} en la modalidad: ${this.modalidad}`);
+        title.textContent = `Bienvenidos al curso de ${this.nombre} division ${this.division}`
     }
     this.agregarCritica = function(critica){
-        formulario.addEventListener("submit", (evento) => {
-        evento.preventDefault();
-        if (like || disLike) {
-            console.log('Critica agregada');
-        } else {
-            console.log('No se agrego critica');
-        }
-        // console.log(evento);
-    })
+        formulario.addEventListener('submit', (evento) => {
+            evento.preventDefault();
+            let btnOk = document.querySelector('#checkP').checked
+            let critica = input.value;
+            if (btnOk && input.value !== '') {
+                this.criticasPositivas.push(critica);
+            } else {
+                alert('Algo salio mal')
+            }
+            listOk.innerHTML = ''
+            listOk.innerHTML += `<li>${this.criticasPositivas}</li>`
+            formulario.reset()
+        })
+       console.log(this.criticasPositivas);
 }
     this.calcularRatingPromedio = function(){
-        if (this.criticasPositivas.length > this.criticasNegativas.length){
-            console.log('Criticas mayormente positivas');
-        } else {
-            console.log('Criticas mayormente negativas');
-        }
+        
     }
     this.mostrarInformacion = function(){
-        console.log(`Bienvenidos al curso de ${this.nombre} en la modalidad: ${this.modalidad}`);
-        // console.log(`Criticas: ${this.criticas}`);
-        // console.log(this.calcularRatingPromedio);
+       
     }
-}
 
+}
 
 let curso1 = new Curso('Programacion', "Online", "Ramallo")
 curso1.presentacion()
 curso1.agregarCritica()
-// curso1.calcularRatingPromedio()
+curso1.calcularRatingPromedio()
 curso1.mostrarInformacion()
